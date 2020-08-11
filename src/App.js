6d2ee0dad5cbd9b9d7268a5b64ee44f652c7428c
Obasa-Oluwatobiloba <ptoblex@gmail.com>
2020-08-11 15:10:49 +0100

    update our menu-item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <Switch>
+      <Route  path='/' component={Homepage} />      
+      <Route  path='/hats' component={HatsPage} />
+      </Switch>
+      
     </div>
   );
 }
