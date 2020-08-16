eba5bd62df364830577e22baea85f42ddb77148c
Obasa-Oluwatobiloba <ptoblex@gmail.com>
2020-08-17 00:03:25 +0100

    finishing sign in and signUp page

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss"
 import FormInput from "../form-input/form-input.component";
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {signInWithGoogle, auth} from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -11,9 +11,16 @@ class SignIn extends React.Component {
     this.state = {};
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const {email, password} = this.state;
+    try{
+        await auth.signInWithEmailAndPassword(email, password)
+        this.setState({ email: "", password: "" });
+    } catch(error){
+        console.error(error);
+    }
+
   };
 
   handleChange = (event) => {
@@ -42,7 +49,7 @@ class SignIn extends React.Component {
           <FormInput
             name='password'
             type='password'
-            value={this.state.email}
+            value={this.state.password}
             handleChange={this.handleChange}
             //label='password'
             required
