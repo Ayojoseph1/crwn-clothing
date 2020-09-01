d00e480680cae10be062bac10c44abe4386dc539
Obasa-Oluwatobiloba <ptoblex@gmail.com>
2020-09-01 11:52:54 +0100

    added cartItem and removeItem functionality, built the checkout page

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/4.3 crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import {createStructuredSelector} from 'reselect'
+
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
-
+import {selectCartHidden} from '../../redux/cart/cart.selector'
+import {selectCurrentUser} from '../../redux/user/user.selector'
 import "./header.styles.scss";
+
 
 const Header = ({ currentUser, hidden }) => (
   <div className="header">
@@ -36,9 +40,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden,
+const mapStateToProps = (createStructuredSelector )({
+  currentUser:selectCurrentUser ,
+  hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
